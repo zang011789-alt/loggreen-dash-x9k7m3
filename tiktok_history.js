53b84254cbb3cce75284a2d5a06684225e10a3b6
zang011789-alt <zang011789@gmail.com>
2026-08-27 05:34:05 +0900

    auto: 2026-08-27 05:34 data update

--- tiktok_history.js
+++ tiktok_history.js
@@ -50650,7 +50650,7 @@ window.TIKTOK_HISTORY = {
       ]
     },
     "ridermune": {
-      "scraped_at": "2026-08-27 05:02",
+      "scraped_at": "2026-08-27 05:32",
       "summary": {
         "spend": 561192,
         "revenue": 26911,
@@ -50828,23 +50828,23 @@ window.TIKTOK_HISTORY = {
   },
   "2026-08-27": {
     "ridermune": {
-      "scraped_at": "2026-08-27 05:03",
+      "scraped_at": "2026-08-27 05:34",
       "summary": {
-        "spend": 76542,
-        "revenue": 24160,
-        "roas": 0.32
+        "spend": 83467,
+        "revenue": 24080,
+        "roas": 0.29
       },
       "campaigns": [
         {
           "name": "tk_kd_spc_020",
           "status": "active",
           "budget": 300000,
-          "cpa": 39573,
-          "spend": 39573,
-          "revenue": 13005,
-          "roas": 3043.0,
-          "cpc": 73,
-          "ctr": 2.4,
+          "cpa": 41664,
+          "spend": 41664,
+          "revenue": 12959,
+          "roas": 3215.0,
+          "cpc": 75,
+          "ctr": 2.33,
           "clicks": 0,
           "impressions": 0,
           "cpm": 0,
@@ -50854,12 +50854,12 @@ window.TIKTOK_HISTORY = {
           "name": "tk_kd_spc_029",
           "status": "active",
           "budget": 399300,
-          "cpa": 36969,
-          "spend": 36969,
-          "revenue": 11155,
-          "roas": 3314.0,
-          "cpc": 365,
-          "ctr": 11.01,
+          "cpa": 41803,
+          "spend": 41803,
+          "revenue": 11121,
+          "roas": 3759.0,
+          "cpc": 435,
+          "ctr": 11.57,
           "clicks": 0,
           "impressions": 0,
           "cpm": 0,
