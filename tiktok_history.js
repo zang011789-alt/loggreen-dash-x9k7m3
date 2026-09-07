f638b79ebc60ca5d7c4eae13fda069fabe50c2d8
zang011789-alt <zang011789@gmail.com>
2026-09-07 12:34:06 +0900

    auto: 2026-09-07 12:34 data update

--- tiktok_history.js
+++ tiktok_history.js
@@ -54053,7 +54053,7 @@ window.TIKTOK_HISTORY = {
   },
   "2026-09-06": {
     "outcoma": {
-      "scraped_at": "2026-09-07 12:02",
+      "scraped_at": "2026-09-07 12:32",
       "summary": {
         "spend": 4162371,
         "revenue": 115647,
